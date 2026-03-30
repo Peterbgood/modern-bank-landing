878c63e66ba58eb24d59c3940e8ddbf2bfd1376c
Peter Good <peterwgood77@gmail.com>
2026-03-30 09:41:23 -0400

    add yml

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // If we are building for production (GitHub), use the sub-folder.
+    // If we are developing locally, use the root.
+    base: command === 'build' ? '/modern-bank-landing/' : '/',
+  }
 })
