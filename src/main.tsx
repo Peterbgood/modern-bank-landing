7da7c59a594e355f1e92834d256ced8fe06f8f4d
Peter Good <peterwgood77@gmail.com>
2026-03-30 10:04:46 -0400

    fix: dynamic basename for local and prod

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+// This detects if we are on GitHub Pages or Localhost
+const basename = import.meta.env.DEV ? '/' : '/modern-bank-landing';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
